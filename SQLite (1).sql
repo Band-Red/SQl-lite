1bb3d2c30ff02ae699109fee564685758393d4eb
Fhahad <119998882+Fhahad@users.noreply.github.com>
2023-03-18 00:52:45 +0300

    Add files via upload

---
+++ SQLite (1).sql
@@ -0,0 +1,10 @@
+CrEate TABLE CAR
+(
+Serial_no number(3),Model number(4),Manufacutter varchar(15),Price number(6)
+);
+Create table option(
+Serail_no number(3),option_name varchar(15),Price number(6)
+);
+CREATE TABLE Sale(
+  Salesperson_id Number (9),serial_no number (3),Date number (8),sale_price number (10)
+  );
